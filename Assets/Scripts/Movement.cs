cfb2182054c90b76ff62934e7708b9bd0f5c3a0e
Alanduril <66563565+Alanduril@users.noreply.github.com>
2025-07-29 21:14:25 +0200

    camera and movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,48 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotate;
+    Rigidbody rb;
+    [SerializeField] float thrustStrength = 1f;
+    [SerializeField] float rotateStrength = 1f;
 
     void OnEnable()
     {
         thrust.Enable();
+        rotate.Enable();
     }
-    void Update()
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProccesRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Thrusting");
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+    void ProccesRotation()
+    {
+        float rotationInput = rotate.ReadValue<float>();
+        if (rotationInput > 0)
+        {
+            ApplyRotation(-rotateStrength);
+        }
+        else if (rotationInput < 0)
+        {
+            ApplyRotation(rotateStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
